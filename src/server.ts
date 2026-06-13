6dc7da32bc7f371dc49312c3fa5684a879426b56
Zhehlov Maksim <maksimzhehlov3@gmail.com>
2026-06-13 10:55:26 +0300

    Creating server.ts, checking the connection to the database (successful)

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { sequelize } from './config/db.js';
+import './models/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ Successful connection to the PostgreSQL database!');
+
+    await sequelize.sync({ alter: true });
+    console.log('✅ Models successfully synchronized with the database. Tables created!');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Error connecting to the database:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
